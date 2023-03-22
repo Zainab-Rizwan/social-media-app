af33c227c87408a0719fd46b9cfc18eb75223df8
Zainab-Rizwan <zainabriz0027@gmail.com>
2023-03-22 23:33:48 +0500

    MongoDB setup

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import multer from "multer";
 import helmet from "helmet";
+import morgan from "morgan";
 import path from 'path';
 import { fileURLToPath } from 'url';
 
