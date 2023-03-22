782ec51e16acaf8e3c95eedfd365d68887a85cc1
Zainab-Rizwan <zainabriz0027@gmail.com>
2023-03-23 03:13:59 +0500

    Authorization and Authentication

--- server/index.js
+++ server/index.js
@@ -8,20 +8,23 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { register } from "./controllers/auth.js";
+import authRoutes from "./routes/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config();
+dotenv.config({ path: "./.env" })
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json({ limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
@@ -33,4 +36,22 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
+
+const PORT = 3001 || 6001;
+const URL = 'mongodb+srv://zainabriz0027:L9s1p7HzRN270X8j@cluster0.psbro4c.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
+
 
