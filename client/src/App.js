b3e3eb98025fa18e41e22a953789593efe000538
Zainab-Rizwan <zainabriz0027@gmail.com>
2023-03-24 14:36:47 +0500

    Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme";
+import Navbar from "scenes/navbar";
 
 function App() {
   const mode = useSelector((state) => state.mode);
@@ -20,14 +21,8 @@ function App() {
           <CssBaseline />
           <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route
-              path="/home"
-              element={isAuth ? <HomePage /> : <Navigate to="/" />}
-            />
-            <Route
-              path="/profile/:userId"
-              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
-            />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile:/userId" element={<ProfilePage />} />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
